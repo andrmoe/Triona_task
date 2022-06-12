725ab7e95e8f52a77db71a3ff715a9566ef4af28
andrmoe <andreas.moe@live.com>
2022-06-12 15:52:40 +0200

    RestAPI finished

--- client/src/Gui.java
+++ client/src/Gui.java
@@ -1,20 +1,25 @@
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-
-import java.util.ArrayList;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.ByteBuffer;
+import java.time.Duration;
 import java.util.HashMap;
+import java.util.Map;
+import java.net.http.HttpClient;
+import java.util.concurrent.Flow;
 
-import static java.awt.Component.CENTER_ALIGNMENT;
+import static java.time.temporal.ChronoUnit.SECONDS;
 
-class Response {
+class Person {
     private String name;
     private String address;
     private String phoneNumber;
     private String dateOfBirth;
-    private String backwardsName;
-    private String phoneNumberSum;
-    private String dateOfBirthLeapYearDescription;
 
     public String getName() {
         return name;
@@ -33,123 +38,184 @@ class Response {
     }
 
     public String getBackwardsName() {
-        return backwardsName;
+        return (new StringBuilder(name)).reverse().toString();
     }
 
     public String getPhoneNumberSum() {
-        return phoneNumberSum;
+        return Integer.toString( phoneNumber.chars().map(Character::getNumericValue).sum() );
     }
 
     public String getDateOfBirthLeapYearDescription() {
-        return dateOfBirthLeapYearDescription;
-    }
-
-    public Response(String name, String address, String phoneNumber, String dateOfBirth) {
-        this.name = name;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.dateOfBirth = dateOfBirth;
-        this.backwardsName = (new StringBuilder(name)).reverse().toString();
-        this.phoneNumberSum = Integer.toString(phoneNumber.chars().map(Character::getNumericValue).sum());
-
+        if (dateOfBirth.length() < 4) {
+            return "";
+        }
         int YearOfBirth = Integer.parseInt(dateOfBirth.substring(0, 4));
-        String textIfLeapYear = "You were born in a leap year";
-        String textIfNotLeapYear = "You were not born in a leap year";
+        String textIfLeapYear = "Person was born in a leap year";
+        String textIfNotLeapYear = "Person was not born in a leap year";
 
         if (YearOfBirth % 400 == 0) {
-            this.dateOfBirthLeapYearDescription = textIfLeapYear;
+            return textIfLeapYear;
         } else if (YearOfBirth % 100 == 0) {
-            this.dateOfBirthLeapYearDescription = textIfNotLeapYear;
+            return textIfNotLeapYear;
         } else if (YearOfBirth % 4 == 0) {
-            this.dateOfBirthLeapYearDescription = textIfLeapYear;
+            return textIfLeapYear;
         } else {
-            this.dateOfBirthLeapYearDescription = textIfNotLeapYear;
+            return textIfNotLeapYear;
         }
+    }
+
+    public String toJSON() {
+        return "{\"name\":\""+name+"\",\"address\":\""+address+"\",\"phoneNumber\":\""+
+                phoneNumber+"\",\"dateOfBirth\":\""+dateOfBirth+"\"}";
+    }
+
+    public void submit() {
+        try {
+            HttpRequest request = HttpRequest.newBuilder().uri(new URI("http://localhost:8000/person"))
+                    .timeout(Duration.of(10, SECONDS))
+                    .POST(HttpRequest.BodyPublishers.ofString(toJSON()))
+                    .header("Content-Type", "application/json").build();
+
+            HttpResponse<String> httpResponse = HttpClient.newBuilder()
+                    .build()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(httpResponse.body());
+
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+    }
+
+    public Person search() throws URISyntaxException, IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder().uri(new URI("http://localhost:8000/person?name="+name.replace(' ','_')))
+                .timeout(Duration.of(10, SECONDS)).GET().build();
+        HttpResponse<String> httpResponse = HttpClient.newBuilder()
+                .build()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        return new Person(httpResponse.body());
+    }
+
+    public Person(String name, String address, String phoneNumber, String dateOfBirth) {
+        this.name = (name != null ? name : "");
+        this.address = (address != null ? address : "");
+        this.phoneNumber = (phoneNumber != null ? phoneNumber : "");
+        this.dateOfBirth = (dateOfBirth != null ? dateOfBirth : "");
+    }
+    public Person(String json) {
+        // Remove curly braces
+
+        json = json.substring(1, json.length()-1);
+
+        if (json.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] keysAndValues = json.split(",");
+
+        for (String keyAndValue: keysAndValues) {
+            String[] param = keyAndValue.split(":");
+            switch (param[0].substring(1, param[0].length()-1)) {
+                case "name" -> this.name = param[1].substring(1, param[1].length()-1);
+                case "address" -> this.address = param[1].substring(1, param[1].length()-1);
+                case "phoneNumber" -> this.phoneNumber = param[1].substring(1, param[1].length()-1);
+                case "dateOfBirth" -> this.dateOfBirth = param[1].substring(1, param[1].length()-1);
+            }
+        }
+    }
+
+    public String toString() {
+        return "Name: %s, Address: %s, Phone Number: %s, Date of Birth: %s".formatted(name, address, phoneNumber, dateOfBirth);
     }
 }
 
 class Gui {
 
-    public static void main(String args[]){
-        JFrame frame = new JFrame("Triona");
+    JFrame frame = new JFrame("Triona");
+    JPanel panel = new JPanel();
+    JLabel responseLabel = new JLabel();
+
+    JLabel NameResultLabel = new JLabel();
+    JLabel AddressResultLabel = new JLabel();
+    JLabel PhoneNumberResultLabel = new JLabel();
+    JLabel DateOfBirthResultLabel = new JLabel();
+    JLabel BackwardsNameLabel = new JLabel();
+    JLabel PhoneNumberSumLabel = new JLabel();
+    JLabel LeapYearLabel = new JLabel();
+    JLabel nameLabel = new JLabel();
+    JTextField nameField = new JTextField();
+    JLabel AddressLabel = new JLabel();
+    JTextField addressField = new JTextField();
+    JTextField phoneNumberField = new JTextField();
+    JLabel dateOfBirthLabel = new JLabel();
+    JTextField dateOfBirthField = new JTextField();
+    JButton submitButton = new JButton("Submit");
+    BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+
+
+    public Gui() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,600);
 
-        JPanel panel = new JPanel();
-        JLabel responseLabel = new JLabel();
 
-        JLabel NameResultLabel = new JLabel();
-        JLabel AddressResultLabel = new JLabel();
-        JLabel PhoneNumberResultLabel = new JLabel();
-        JLabel DateOfBirthResultLabel = new JLabel();
-        JLabel BackwardsNameLabel = new JLabel();
-        JLabel PhoneNumberSumLabel = new JLabel();
-        JLabel LeapYearLabel = new JLabel();
-
-
-        BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
 
         panel.setLayout(boxLayout);
 
-        JLabel nameLabel = new JLabel();
+
         nameLabel.setText("Name");
         panel.add(nameLabel);
 
-        JTextField nameField = new JTextField();
+
         panel.add(nameField);
 
-        JLabel AddressLabel = new JLabel();
+
         AddressLabel.setText("Address");
         panel.add(AddressLabel);
 
-        JTextField addressField = new JTextField();
+
         panel.add(addressField);
 
         JLabel phoneNumberLabel = new JLabel();
         phoneNumberLabel.setText("Phone Number");
         panel.add(phoneNumberLabel);
 
-        JTextField phoneNumberField = new JTextField();
+
         panel.add(phoneNumberField);
 
-        JLabel dateOfBirthLabel = new JLabel();
+
         dateOfBirthLabel.setText("Date of Birth");
         panel.add(dateOfBirthLabel);
 
-        JTextField dateOfBirthField = new JTextField();
+
         panel.add(dateOfBirthField);
 
-        JButton submitButton = new JButton("Submit");
+
         ButtonModel submitButtonModel = submitButton.getModel();
         submitButtonModel.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 if (submitButtonModel.isPressed()) {
+                    getInput().submit();
+                    clearOutput();
                     responseLabel.setText("Data submitted");
-                    NameResultLabel.setText("");
-                    AddressResultLabel.setText("");
-                    PhoneNumberResultLabel.setText("");
-                    DateOfBirthResultLabel.setText("");
-                    BackwardsNameLabel.setText("");
-                    PhoneNumberResultLabel.setText("");
-                    LeapYearLabel.setText("");
                 }
             }
         });
         panel.add(submitButton);
-        JButton searchButton = new JButton("Search");
+        JButton searchButton = new JButton("Search (Name Only)");
         ButtonModel searchButtonModel = searchButton.getModel();
         searchButtonModel.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 if (searchButtonModel.isPressed()) {
-                    Response response = new Response(nameField.getText(), addressField.getText(), phoneNumberField.getText(), dateOfBirthField.getText());
-                    responseLabel.setText("Results:");
-                    NameResultLabel.setText("Name: "+response.getName());
-                    AddressResultLabel.setText("Address: "+response.getAddress());
-                    PhoneNumberResultLabel.setText("Phone Number: "+response.getPhoneNumber());
-                    DateOfBirthResultLabel.setText("Date of Birth: "+response.getDateOfBirth());
-                    BackwardsNameLabel.setText("Backwards Name: "+response.getBackwardsName());
-                    PhoneNumberSumLabel.setText("Phone Number Sum: "+response.getPhoneNumberSum());
-                    LeapYearLabel.setText(response.getDateOfBirthLeapYearDescription());
+                    try {
+                        Person response = getInput().search();
+                        System.out.println(response);
+                        responseLabel.setText("Results:");
+                        setOutput(response);
+                    } catch (Exception exception) {
+                        System.out.println(exception);
+                    }
+
+
+
                 }
             }
         });
@@ -167,5 +233,35 @@ class Gui {
 
         frame.getContentPane().add(panel);
         frame.setVisible(true);
+    }
+
+    public Person getInput() {
+        return new Person(nameField.getText(), addressField.getText(), phoneNumberField.getText(), dateOfBirthField.getText());
+    }
+
+    public void setOutput(Person person) {
+        NameResultLabel.setText("Name: "+person.getName());
+        AddressResultLabel.setText("Address: "+person.getAddress());
+        PhoneNumberResultLabel.setText("Phone Number: "+person.getPhoneNumber());
+        DateOfBirthResultLabel.setText("Date of Birth: "+person.getDateOfBirth());
+        BackwardsNameLabel.setText("Backwards Name: "+person.getBackwardsName());
+        PhoneNumberSumLabel.setText("Phone Number Sum: "+person.getPhoneNumberSum());
+        LeapYearLabel.setText(person.getDateOfBirthLeapYearDescription());
+    }
+
+    public void clearOutput() {
+        responseLabel.setText("");
+        NameResultLabel.setText("");
+        AddressResultLabel.setText("");
+        PhoneNumberResultLabel.setText("");
+        DateOfBirthResultLabel.setText("");
+        BackwardsNameLabel.setText("");
+        PhoneNumberSumLabel.setText("");
+        LeapYearLabel.setText("");
+    }
+
+    public static void main(String args[]){
+        Gui gui = new Gui();
+
     }
 }
--- server/src/Main.java
+++ server/src/Main.java
@@ -1,7 +1,53 @@
-public class Main {
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import com.sun.net.httpserver.*;
 
 
-    public static void main(String[] args) {
+class Server {
 
+    public static  void main(String args[]) {
+
+        HttpHandler handler = new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+
+                URI uri = exchange.getRequestURI();
+
+                if (exchange.getRequestMethod().equalsIgnoreCase("GET") && uri.getQuery().contains("=")) {
+
+                    String[] stringParams = uri.getQuery().split("=");
+                    if (!stringParams[0].equals("name")) {
+                        exchange.sendResponseHeaders(400, -1);
+                    } else {
+                        String name = stringParams[1];
+                        Headers responseHeaders = exchange.getResponseHeaders();
+                        responseHeaders.set("Content-Type", "application/json");
+                        OutputStream responseBody  = exchange.getResponseBody();
+                        exchange.sendResponseHeaders(200, 0);
+                        responseBody.write("{\"name\":\"Andreas\",\"address\":\"Munkegata 1\",\"phoneNumber\":\"01234567\",\"dateOfBirth\":\"2000-01-01\"}".getBytes());
+                        responseBody.close();
+                    }
+                } else if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
+                    System.out.println(new String(exchange.getRequestBody().readAllBytes()));
+                    exchange.sendResponseHeaders(200, -1);
+                } else {
+                    exchange.sendResponseHeaders(400, -1);
+                }
+            }
+        };
+
+        String context = "/person";
+
+        try {
+
+            HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+            httpServer.createContext(context, handler);
+            httpServer.setExecutor(null);
+
+            httpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
